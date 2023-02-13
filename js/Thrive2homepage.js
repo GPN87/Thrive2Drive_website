26d25636cf492757cccd9be2dcc892c87245b306
GPN87 <gavin.payne87@gmail.com>
2023-02-13 16:32:28 +1100

    js folder

---
+++ js/Thrive2homepage.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.has-animation').each(function(index) {
+    $(this).delay($(this).data('delay')).queue(function(){
+      $(this).addClass('animate-in');
+    }
+  }
+}
+$('.carousel .vertical .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=1;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
